f3ccc5292d29082219c6818549720598649c7cf9
OmarHafdioui <50831701+OmarHafdioui@users.noreply.github.com>
2019-10-15 11:13:02 +0100

    smallsimp

--- Design & Semantics/hw1.scm
+++ Design & Semantics/hw1.scm
@@ -1,88 +1,143 @@
 #lang racket
-
-;;; Alan Lyne 15468498
-
-;;;Differentiation
+    
 (define d
   (λ (e)
     (cond ((number? e) 0)
 	  ((equal? e 'x) 1)
-          ((equal? e '-x) -1)
 	  (else
 	   (let ((op (car e)) (args (cdr e)))
 	     (apply (lookup op d-op-table) args))))))
 
+
+(define lookup
+  (λ (op table)
+    (if (equal? op (caar table))
+	(cadar table)
+	(lookup op (cdr table)))))
+
 (define d-op-table
-  (list(list '+ (λ (u v) (list '+ (d u) (d v)))) ;Working
+  (list(list '+ (λ (u v) (simp+ (d u) (d v)))) 
        
-       (list '- (λ (u v) (list '- (d u) (d v)))) ;Working
+       (list '- (λ (u v) (simp- (d u) (d v)))) 
        
        (list '* (λ (u v)
-          (list '+ (list ' * u (d v)) (list ' * v (d u))))) ;Working
+          (simp+ (simp* u (d v)) (simp* v (d u))))) 
        
        (list '/ (λ (u v)
-          (list '/ (list '- (list ' * v (d u)) (list ' * u (d v))) (list ' * v v)))) ;Working
+          (simp/ (simp- (simp* v (d u)) (simp* u (d v))) (simp* v v)))) ;Working
 
-       (list 'expt (λ (u v) (list 'expt (list '* v  u) (- v 1)))) ;Working
+
+       (list 'log (λ (u) (simp* (simp/ 1  u) (d u))))
        
-       (list 'exp (λ(u) (list '* (d u) (list 'exp u)))) ;Wokring
+       (list 'recip (λ (v)                      
+          (list '/ (simp- 0 (simp* 1 (d v))) (simp* v v)))) 
 
-       (list 'log (λ (u) (list '* (list '/ 1  u) (d u)))) ;Working
+       (list 'sin (λ (u v) (list '* (d u) 'cos (list u)))) 
        
-       (list 'recip (λ (v)
-          (list '/ (list '- 0 (list ' * 1 (d v))) (list ' * v v)))) ;Working
+       (list 'cos (λ (u v) (list '* (d u) '* -1 'sin (list u))))
+
+       (list 'expt (λ (u v) (simpexpt (simp* v  u) (simp- v 1))))
        
-       (list 'sin (λ (u v) (list '* (d u) 'cos (list u)))) ;Working
-       
-       (list 'cos (λ (u v) (list '* (d u) '* -1 'sin (list u)))))) ;Working
+       (list 'exp (λ(u) (simp* (d u) (list 'exp u))))
+
+))
 
 
 
-;;;TTMS-Eval
 (define ttms-eval
-  (λ (a z)
-    (cond ((number? a) a)
-	  ((equal? a 'x) z)
+  (λ (e v)
+    (cond ((number? e) e)
+	  ((equal? e 'x) v)
 	  (else
-	   (let ((op (car a))
-		 (args (cdr a)))
-	     (apply (lookup op ttml-op-table)
-		    (map (λ (aa) (ttms-eval aa z)) args)))))))
+	   (let ((op (car e))
+		 (args (cdr e)))
+	     (apply (ttml-lookup op ttml-op-table)
+		    (map (λ (ee) (ttms-eval ee v)) args)))))))
 
 (define ttml-op-table
-  (list (list '+ (λ (x1 x2) (+ x1 x2))) ;Working
-	(list '* (λ (x1 x2) (* x1 x2))) ;Working
+  (list (list '+ (λ (u v) (simp+ u v)))
 
-        (list '- (λ (x1 x2) (- x1 x2))) ;Working
-        (list '/ (λ (x1 x2) (/ x1 x2))) ;Working
-        (list 'expt (λ (x1 x2) (expt x1 x2))) ;Working
-
-        (list 'recip (λ (x1) (/ 1 x1))) ;Working
-        (list 'log log); Working
+        (list '- (λ (u v) (simp- u v)))
         
-	(list 'sin sin) ;Working
-	(list 'cos cos) ;Working
-	(list 'exp exp))) ;Working#
+	(list '* (λ (u v) (simp* u v)))
 
+        (list '/ (λ (u v) (simp/ u v)))
+        
+        (list 'expt (λ (u v) (expt u v))) 
 
-;;;Lookup Table
-(define lookup (λ (x alist) (cadr (assoc x alist))))
+        (list 'recip (λ (u) (/ 1 u)))
+        
+        (list 'log log)
+        
+	(list 'exp exp)
 
-'Differentiation
+        (list 'sin sin)
+        
+	(list 'cos cos)
+        ))
+
+(define ttml-lookup (λ (x alist) (cadr (assoc x alist))))
+
+(define simp+ (λ (x y)
+    (cond ((and (number? x) (number? y))
+	   (+ x y))
+	  ((equal? x 0) y)
+	  ((equal? y 0) x)
+          ((equal? y x) (simp* 2 x))
+	  (else (list '+ x y)))))
+(define simp- (λ (x y)
+    (cond ((and (number? x) (number? y))
+	   (- x y))
+	  ((equal? x 0) (list '- y))
+	  ((equal? y 0) x)
+          ((equal? y x) 0)
+	  (else (list '- x y)))))
+(define simp* (λ (x y)
+    (cond ((and (number? x) (number? y))
+	   (* x y))
+	  ((equal? x 0) 0)
+	  ((equal? y 0) 0)
+          ((equal? x 1) y)
+	  ((equal? y 1) x)
+	  (else (list '* x y)))))
+(define simp/ (λ (x y)
+    (cond ((and (number? x) (number? y))
+	   (/ x y))
+	  ((equal? x 0) 0)
+	  (else (list '/ x y)))))
+(define simpexpt (λ (x y)
+    (cond ((and (number? x) (number? y))
+	   (expt x y))
+          ((equal? y 0) 1)
+	  ((equal? x 0) 0)
+          ((equal? y 1) x)
+          ((equal? x 1) 1)
+	  (else (list 'expt x y)))))
+(define simpexp (λ (x y)
+    (cond ((and (number? x) (number? y))
+	   (/ x y))
+	  ((equal? x 0) 0)
+	  (else (list '/ x y)))))
+
+;;; test cases
+
+'differentiation
+(d '(- (- x 1) (- x -1)))
 (d '(* (+ x 1) (+ x -1)))
-(d '(/ (+ x 1) (+ x -1)))
-(d '(log (+ x 1) ))
-(d '(expt (* x 1) 2 ))
-(d '(exp (* x 1)))
-(d '(recip (* x 1)))
-(d '(sin (* x 1)2))
-(d '(cos (* x 1)2))
-'TTMS-EVAL
-(ttms-eval '(+ 1 (* x 2)) 7)
-(ttms-eval '(/ 1 (* x 2)) 7)
-(ttms-eval '(log (* x 2)) 7)
-(ttms-eval '(expt 15 (* x 2)) 7)
-(ttms-eval '(exp (* x 2)) 7)
-(ttms-eval '(recip (* x 2)) 7)
-(ttms-eval '(sin (* x 2)) 7)
-(ttms-eval '(cos (* x 2)) 7)
+(d '(/ 5 (+ x -1)))
+(d '(log x))
+(d '(expt (* 2 x) 2 ))
+(d '(exp x))
+(d '(recip (* x x)))
+(d '(sin (* 7 x)2))
+(d '(cos (* 2 1)2))
+'ttms-eval
+(ttms-eval '(+ 1 (* 7 x)) 4)
+(ttms-eval '(- 1 (* 7 x)) 4)
+(ttms-eval '(/ 1 x) 2)
+(ttms-eval '(log (* 4 x)) 4)
+(ttms-eval '(expt 15 (* 2 x)) 4)
+(ttms-eval '(exp (* 9 x)) -4)
+(ttms-eval '(recip (* x x)) -7)
+(ttms-eval '(sin (* x x)) 2)
+(ttms-eval '(cos (* 1 x)) 4)
