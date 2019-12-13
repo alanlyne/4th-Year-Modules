72cd67bc33973cce0e650de406fec0204281d968
Alan Lyne <alanlyne@outlook.com>
2019-12-13 14:46:00 +0000

    CS424 second assignment

--- Design & Semantics/hw2/sugar.hs
+++ Design & Semantics/hw2/sugar.hs
@@ -1,23 +1,26 @@
 import Data.List
 
-data Term = Variable String | Application Term Term | Lambda String Term deriving Show
+data Term = Variable String 
+            | Application Term Term 
+            | Lambda String Term 
+            deriving Show
 
-data Term' = Variable' String | Application' Term' [Term'] | Lambda' [String] Term' deriving Show
+data Term' = Variable' String 
+            | Application' Term' [Term'] 
+            | Lambda' [String] Term' 
+            deriving Show
 
 desugar :: Term' -> Term
 
-desugar (Variable' x) = Variable x
+desugar (Variable' c) = Variable c
+desugar (Application' t []) = desugar(t)
+desugar (Lambda' [] b) = desugar (b)
+desugar (Lambda' (x:xs) b) = Lambda x (desugar(Lambda' xs b))
 
-desugar(Application' t []) = desugar(t)
+desugar (Application' a b) = do
+    let l = last(b)
+    let first = init(b)
+    let ter = desugar(Application' a first)
+    let term = desugar(l)
 
-desugar (Lambda' [] x2) = desugar (x2)
-
-desugar (Lambda' (x:xs) x2) = Lambda x (desugar(Lambda' xs x2))
-
-desugar (Application' x1 x2) = do
-    let las = last(x2)
-    let fir = init(x2)
-    let term = desugar(Application' x1 fir)
-    let ter = desugar(las)
-
-    Application term ter
+    Application ter term
