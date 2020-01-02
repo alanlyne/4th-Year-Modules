6158e7681643e45e6a26d02631d4193726ddab69
Alan Lyne <alanlyne@outlook.com>
2020-01-02 14:10:46 +0000

    Updated hw3 for CS424

--- Design & Semantics/hw3/hw3.pl
+++ Design & Semantics/hw3/hw3.pl
@@ -5,3 +5,7 @@ mushed([X|Xs], Ys, [X|Zs]) :-
 
 mushed(Xs, [Y|Ys], [Y|Zs]) :- 
     mushed(Xs, Ys, Zs).
+
+circular([_|_], []). 
+circular([H|L1], L2) :- 
+    append([H|L1], L3, L2), circular([H|L1], L3).
