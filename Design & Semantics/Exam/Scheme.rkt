8f6ac1d13217019faa37f3ede277f6bea127c365
Alan Lyne <alanlyne@outlook.com>
2020-01-04 15:04:55 +0000

    Scheme 2019 - Working

--- Design & Semantics/Exam/Scheme.rkt
+++ Design & Semantics/Exam/Scheme.rkt
@@ -1,5 +1,68 @@
 #lang racket
 
+; Jan 2019
+;Define a Scheme function SCATTER-GATHER which takes two
+;arguments, a list INDICES of indices and a list VALS of values,
+;and returns a list of the same length as INDICES but with each
+;value K replaced by the K-th element of VALS, or if that is out of
+;range, by #f.
+
+;(define (sg indices val)
+;  (if (null? indices)
+;      '()
+;      (with-handlers ([exn:fail? (lambda (exn) #f)])
+;      (cons (list-ref val (car ind)) (sg (cdr ind) val)))))
+
+
+(define (scatterGather indices values)
+  (cond ((null? indices)
+         '())
+        
+        ((>(+(length values) 2)(car indices)) 
+        (cons (list-ref values (car indices))(scatterGather (cdr indices) values)))
+        
+        (else
+         (cons '#f(scatterGather (cdr indices) values)))))
+ 
+
+(scatterGather '(0 1 4 1 1 7 2) '(a b c d e))
+;=> (a b e b b #f c)
+#|
+(define (scatterGather indices values)
+  (if (null? ind)
+      '()
+      (with-handlers ([exn:fail? (lambda (exn) #f)])
+      (cons (list-ref values (car indices)) (scatterGather (cdr indices) values)))))
+|#
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ 
+;Autumn 2019
+(define (plusevens  lst)
+  (if (> 3 (length lst)) ; length less than 3
+            (car lst)                    
+            (+ (car lst) ; add first element of new list           
+               (plusevens (cddr lst))))) ; pass back the list with the top two elements taken off it
+#| 
+
+Example 
+(plusevens '(1 20 300 4000 50000 600000)) => 50301
+
++ 1
+lst => (300 4000 50000 600000)
+
++300
+lst => (50000 600000)
+(lentgth of lst < 3)
+(car lst)
+50000
+
+add all together
+
+NB: (car lst) == (+ (car lst)) why? why + optional and still works?
+|#
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;Jan 2018
 ;Define a Scheme function tear which takes two arguments, a
 ;predicate p? and a list xs, and returns a list of two lists, the
@@ -13,11 +76,12 @@
 ;The "(λ (x)(not (p? x)))" procedure is applied to each element from first to last.
 ;List returns a newly allocated list.
 
-(tear number? '(a b c 1 2 3 d e f))
+;(tear number? '(a b c 1 2 3 d e f))
 ;=> ((1 2 3) (a b c d e f))
- (tear (lambda (x) (> x 5)) '(1 10 2 12 3 13))
+;(tear (lambda (x) (> x 5)) '(1 10 2 12 3 13))
 ;=> ((10 12 13) (1 2 3))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;Aug 2018
 ;Define a Scheme function map-skip which takes a function and a
@@ -36,5 +100,7 @@
 ;The process happens again until l is empty/null.
 ;"appeand" puts everything together at the end
 
-(map-skip (λ (x) (+ x 1000)) '(1 2 3 4 5 6))
+;(map-skip (λ (x) (+ x 1000)) '(1 2 3 4 5 6))
 ;=> (1001 2 1003 4 1005 6)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
