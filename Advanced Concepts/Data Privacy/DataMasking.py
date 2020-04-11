28669385f6f11c2bfed9b459084f755e4d9a175a
alanlyne <alanlyne@outlook.com>
2020-04-11 10:14:29 +0100

    Slight edits

--- Advanced Concepts/Data Privacy/DataMasking.py
+++ Advanced Concepts/Data Privacy/DataMasking.py
@@ -1,9 +1,11 @@
+# 15468498
+# Alan Lyne
+
 from sklearn import preprocessing
 import numpy as np
 import pandas as pd
-import csv
 
-  
+
 def stand(df):
     names = df.columns
 
@@ -14,24 +16,32 @@ def stand(df):
     scaled_df = scaler.fit_transform(df)
     scaled_df = pd.DataFrame(scaled_df, columns=names)
 
-    #print(scaled_df)
+    # print(scaled_df)
 
     return scaled_df
 
+
 def noise(df):
-    df = pd.DataFrame([[1,2],[3,4]], columns=list('AB'), dtype=float)
     mu = 0
     sigma = 0.1
-    noise = np.random.normal(mu, sigma, [2,2])
+
+    # Creating noise the same size as the dataset
+    noise = np.random.normal(mu, sigma, df.shape)
     signal = df + noise
 
     return signal
 
+
 if __name__ == "__main__":
     # Get dataset
-    df = pd.read_csv("Advanced Concepts\\Data Privacy\\CASCrefmicrodata.csv") 
-    scaled_df = stand(df)
-    signal = noise(df)
+    df = pd.read_csv(r"Advanced Concepts\\Data Privacy\\CASCrefmicrodata.csv")
+    print(df)
 
+    # Normalise/Standardise the data
+    scaled_df = stand(df)
     print(scaled_df)
-    print(signal) #NEEDS WORK
+
+    # Masking Method (Gaussian Noise)
+    signal = noise(scaled_df)
+    print(signal)
+
