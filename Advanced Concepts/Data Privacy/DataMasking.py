edb28a5c74dda5164bf7802eed8a03d8fafb1424
alanlyne <alanlyne@outlook.com>
2020-04-11 11:37:22 +0100

    Euclidean & Inforamtion loss complete

--- Advanced Concepts/Data Privacy/DataMasking.py
+++ Advanced Concepts/Data Privacy/DataMasking.py
@@ -2,8 +2,10 @@
 # Alan Lyne
 
 from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def stand(df):
@@ -32,6 +34,16 @@ def noise(df):
     return signal
 
 
+# Calculate the Eucliane Distance between the original and masked dataset
+def euclidean(df, signal):
+    return (np.linalg.norm(df-signal))
+
+
+# Calculate the information lossed between the masked and prginal set using mean square error
+def infoLoss(df, signal):
+    return np.sqrt(mean_squared_error(df, signal))
+
+
 if __name__ == "__main__":
     # Get dataset
     df = pd.read_csv(r"Advanced Concepts\\Data Privacy\\CASCrefmicrodata.csv")
@@ -44,4 +56,17 @@ if __name__ == "__main__":
     # Masking Method (Gaussian Noise)
     signal = noise(scaled_df)
     print(signal)
+
+    euclideanDist = euclidean(df, signal)
+    print("Euclidean distance between two said series: ", euclideanDist)
+
+    informationLoss = infoLoss(df, signal)
+    print("The calculated information loss is: ", informationLoss)
+
+    # fig=plt.figure()
+    # ax=fig.add_axes([0,0,1,1])
+    # ax.scatter(10,euclideanDist, color = 'r')
+    # ax.scatter(10,informationLoss, color = 'b')
+    # plt.show()
+
 
