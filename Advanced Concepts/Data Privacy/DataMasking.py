ecbac8e7b31034f255abe2530cc3641559f8795a
alanlyne <alanlyne@outlook.com>
2020-04-13 01:03:20 +0100

    Added DBRL

--- Advanced Concepts/Data Privacy/DataMasking.py
+++ Advanced Concepts/Data Privacy/DataMasking.py
@@ -6,6 +6,7 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from math import sqrt
 
 # Normalise/Standardise the dataset
 def stand(df):
@@ -35,15 +36,39 @@ def noise(df):
 
 # Calculate the Disclosure Risk (Euclidean Distance) between the original and masked dataset
 def euclidean(df, signal):
-    euclideanDist = np.linalg.norm(df-signal)
-    print("Euclidean distance between two said series: ", euclideanDist)
-    return pd.Series(np.linalg.norm(df.to_numpy()-signal.to_numpy(), axis=0), index=df.columns)
+    #euclideanDist = np.linalg.norm(df-signal)
+    #return euclideanDist
+    #print("The calculated Disclosure Risk is: ", euclideanDist)
+    #return pd.Series(np.linalg.norm(df.to_numpy()-signal.to_numpy(), axis=0), index=df.columns)
+    return sqrt(sum((df-signal) * (df-signal)))
+
+
+def dbrl(original, masked):
+    original = original.to_numpy()
+    masked = masked.to_numpy()
+    i = 1
+    reindentified = 0
+    while i < len(original):
+        j = 1
+        minDist = 100000
+        minRecord = -1
+        while j < len(masked):
+            if euclidean(original[i,], masked[j,]) < minDist:
+                minDist = euclidean(original[i,], masked[j,])
+                minRecord = j
+            j += 1
+            #print(j)
+        if minRecord == i:
+            reindentified +=1
+        i += 1
+        print(i)
+    return reindentified
 
 
 # Calculate the information lossed between the masked and prginal set using mean square error
 def infoLoss(df, signal):
-    informationLoss = (mean_squared_error(df, signal))
-    print("The calculated information loss is: ", informationLoss)
+    #informationLoss = (mean_squared_error(df, signal))
+    #print("The calculated information loss is: ", informationLoss)
     return pd.Series(np.square(np.subtract(df, signal)).mean())
 
 
@@ -60,15 +85,18 @@ if __name__ == "__main__":
     signal = noise(scaled_df)
     print(signal)
 
-    euclideanDist = euclidean(df, signal)
-    #print("Euclidean distance between two said series: ", euclideanDist)
+    euclideanDist = dbrl(df, signal)
+    print("Euclidean distance between two said series: ", euclideanDist)
 
     informationLoss = infoLoss(df, signal)
-    #print("The calculated information loss is: ", informationLoss)
+    print("The calculated information loss is: ", informationLoss)
  
 
-    informationLoss.plot(style = '.')
-    euclideanDist.plot(style = '.')
+    #informationLoss.plot(style = '.')
+    #euclideanDist.plot(style = '.')
+
+    plt.scatter(informationLoss, euclideanDist)
+    #plt.scatter(euclideanDist)
 
     plt.title("Microaggregation individual ranking dRisk(k)")
     plt.xlabel("dRisk")
